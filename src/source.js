6584f2efacaab576cc7eeb1667e9d7c2fb240120
Shayan <shayan.yazdanmehr@gmail.com>
2016-07-18 17:05:59 +0200

    first commit

---
+++ src/source.js
@@ -0,0 +1,58 @@
+//noinspection JSAnnotator
+/**
+ * Created by shayan on 18.07.16.
+ */
+function changeBackgroundColor() {
+    document.getElementById("wrapper").style.backgroundColor="red";
+}
+
+function createCalendar(){
+
+    //Number of days in each month
+
+    var days = moment().daysInMonth();
+    var wrapper = document.getElementById("wrapper");
+    //create table node
+    var table = document.createElement("table");
+
+    //Adding name of the month and year to the first row
+    var firstRow =document.createElement("tr");
+    var header = document.createElement("th");
+    header.colSpan = "100";
+    header.innerHTML = moment().startOf("month").format("MMMM YYYY");
+    firstRow.appendChild(header);
+    table.appendChild(firstRow);
+
+    var tableRow = document.createElement("tr");
+    for(var i = 0; i <= days; i++){
+        var td = document.createElement("th");
+        td.setAttribute("class","noselect");
+        if ( i > 0){
+            td.innerText = moment().startOf("month").add(i-1,"day").format("D/M");
+        }
+        tableRow.appendChild(td);
+    }
+
+    table.appendChild(tableRow);
+
+    for(var i = 0; i < 24; i++){
+        var tableRow = document.createElement("tr");
+        for(var j = 0; j <= days; j++){
+            var td = document.createElement("td");
+            td.setAttribute("class","noselect");
+            if( j== 0){
+                td.innerText = moment(i.toString(),"hh").format("HH:mm");
+            }
+            tableRow.appendChild(td);
+        }
+        table.appendChild(tableRow);
+    }
+
+    wrapper.appendChild(table);
+}
+function getValue(){
+    this.innerHTML = "Clicked";
+}
+
+
+
